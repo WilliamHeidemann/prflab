a48950101be3c7126e1fbc8f8818b5abeec8904f
William Skou Heidemann <Williamskou2227@gmail.com>
2023-11-05 19:20:49 +0100

    .

--- kernels.c
+++ kernels.c
@@ -394,7 +394,7 @@ void xor_first(int dim, pixel *src, pixel *dst)
         }
     }
 
-    // Transpose using xor
+    // Transpose \ using xor
     for (i = 0; i < dim; i++){
         for (j = i + 1; j < dim; j++){
             pixel *px1 = &dst[RIDX(i, j, dim)];
@@ -447,11 +447,11 @@ void xor2(int dim, pixel *src, pixel *dst) {
         }
     }
 
-    // Transpose using xor
+    // Transpose / using xor
     for (i = 0; i < dim; i++){
         for (j = dim_m_one; j > i; j--){
             pixel *px1 = &dst[RIDX(i, j, dim)];
-            pixel *px2 = &dst[RIDX(j, i, dim)];
+            pixel *px2 = &dst[RIDX(dim_m_one - j, dim_m_one - i, dim)];
             xor(px1, px2);
             xor(px2, px1);
             xor(px1, px2);
