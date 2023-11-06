18f173081050059c2c5b3c37c7de4bbd41a2b67c
William Skou Heidemann <Williamskou2227@gmail.com>
2023-11-06 10:38:45 +0100

    .

--- kernels.c
+++ kernels.c
@@ -564,13 +564,13 @@ void xor6(int dim, pixel *src, pixel *dst){
             dst[a+1] = dst[b-dim];
             dst[b-dim] = temp;
 
-            if (diagonal_index > a + 2) {
+            if (diagonal_index >= a + 2) {
                 temp = dst[a+2];
                 dst[a+2] = dst[b-dim-dim];
                 dst[b-dim-dim] = temp;
             }
 
-            if (diagonal_index > a + 3) {
+            if (diagonal_index >= a + 3) {
                 temp = dst[a+3];
                 dst[a+3] = dst[b-dim-dim-dim];
                 dst[b-dim-dim-dim] = temp;
